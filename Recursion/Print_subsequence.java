3dd7eecbf43e3269c51cb88d71549b49a1c38087
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-02-26 11:21:51 +0530

    Add recursive method to find all subsets of an array
    Implement a recursive helper function to generate all subsets of an array.

--- Recursion/Print_subsequence.java
+++ Recursion/Print_subsequence.java
@@ -1,1 +1,33 @@
 
+class Solution {
+
+    public void helper(int arr[], int idx, int n,
+                       List<List<Integer>> ans,
+                       List<Integer> temp) {
+
+        // base case
+        if (idx == n) {
+            ans.add(new ArrayList<>(temp)); // ✅ copy list
+            return;
+        }
+
+        // NOT TAKE
+        helper(arr, idx + 1, n, ans, temp);
+
+        // TAKE
+        temp.add(arr[idx]);
+        helper(arr, idx + 1, n, ans, temp);
+
+        // ✅ BACKTRACK
+        temp.remove(temp.size() - 1);
+    }
+
+    public List<List<Integer>> subsets(int[] arr) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        helper(arr, 0, arr.length, ans, temp);
+        return ans;
+    }
+}
