9a20c8cec76440bed3e12ed7448d104c6e9b9c54
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-03-16 13:02:13 +0530

    Add sliding window method for substring search
    Implemented sliding window approach to find the first occurrence of a substring.

--- Strings/find_the_index_of_first_occurence_of_string.java
+++ Strings/find_the_index_of_first_occurence_of_string.java
@@ -16,3 +16,23 @@ class Solution {
         return -1;
     }
 }
+
+//sliding window approach and optimal
+class Solution {
+    public int strStr(String s1, String s2) {
+        int p1 = 0;
+        int p2 = s2.length() - 1;
+        while (p2 < s1.length()) {
+            String x = "";
+            for (int i = p1; i <= p2; i++) {
+                x = x + s1.charAt(i);
+            }
+            if (x.equals(s2)) {
+                return p1;
+            }
+            p1++;
+            p2++;
+        }
+        return -1;
+    }
+}
