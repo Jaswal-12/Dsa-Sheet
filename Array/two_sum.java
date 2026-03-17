bec3c790fb31e4afe47b074a0329e207882b433c
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-03-17 14:54:00 +0530

    Implement twoSum method to find indices of numbers

---
+++ Array/two_sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+
+        HashMap<Integer,Integer>mp=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            int x=target-nums[i];
+
+            if(mp.containsKey(x)){
+                return new int[]{i,mp.get(x)};
+            }else{
+                mp.put(nums[i],i);
+            }
+        }
+
+        return new int[]{-1,-1};
+    }
+}
