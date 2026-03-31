7558334185ff358aff3e2e41d07ad065d3ae4c52
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-04-01 00:05:11 +0530

    Implement wonderfulSubstrings method for counting

--- Strings/Number_of_wonderful_strings.java
+++ Strings/Number_of_wonderful_strings.java
@@ -1,1 +1,29 @@
+// optimal-approach
+class Solution {
+    public long wonderfulSubstrings(String word) {
+        
+        long count = 0;
+        int mask = 0;
 
+        int[] freq = new int[1024]; // 2^10
+        freq[0] = 1;
+
+        for (char ch : word.toCharArray()) {
+
+            int bit = ch - 'a';
+            mask ^= (1 << bit); // toggle
+
+            // same mask
+            count += freq[mask];
+
+            // one bit different
+            for (int i = 0; i < 10; i++) {
+                count += freq[mask ^ (1 << i)];
+            }
+
+            freq[mask]++;
+        }
+
+        return count;
+    }
+}
