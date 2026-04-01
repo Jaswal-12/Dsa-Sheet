1466b1783ce2b09ad2a3ca71491213fa63ed116a
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-04-01 17:27:41 +0530

    Update How Many Numbers Are Smaller Than the Current Number.java

--- HashMap/How Many Numbers Are Smaller Than the Current Number.java
+++ HashMap/How Many Numbers Are Smaller Than the Current Number.java
@@ -22,3 +22,39 @@ class Solution {
           return ans;
     }
 }
+
+
+//brute force
+import java.util.*;
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+        // Step 1: frequency map
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        }
+
+        // Step 2: sort unique elements
+        ArrayList<Integer> list = new ArrayList<>(mp.keySet());
+        Collections.sort(list);
+
+        // Step 3: prefix sum (count of smaller numbers)
+        int sum = 0;
+        HashMap<Integer, Integer> smaller = new HashMap<>();
+
+        for(int num : list){
+            smaller.put(num, sum);
+            sum += mp.get(num);
+        }
+
+        // Step 4: build result
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = smaller.get(nums[i]);
+        }
+
+        return ans;
+    }
+}
