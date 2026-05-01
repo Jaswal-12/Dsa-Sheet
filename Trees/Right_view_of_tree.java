eda9659ef1b90f65d1883cd5ca384746b0a54c31
Karan Jaswal <118061359+Jaswal-12@users.noreply.github.com>
2026-05-02 00:38:31 +0530

    Create Right_view_of_tree.java

---
+++ Trees/Right_view_of_tree.java
@@ -0,0 +1,35 @@
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+      List<Integer>ans=new ArrayList<>();
+      
+      if(root==null) return ans;
+
+      Queue<TreeNode>q=new LinkedList<>();
+      q.add(root);
+
+
+      while(!q.isEmpty()){
+         int s=q.size();
+
+         for(int i=0;i<s;i++){
+            TreeNode node=q.poll();
+            if(i==s-1){
+                ans.add(node.val);
+            }
+
+            if(node.left!=null){
+                q.add(node.left);
+            }
+
+            if(node.right!=null){
+                q.add(node.right);
+            }
+         }
+      }
+
+      return ans;
+
+    }
+}
+
